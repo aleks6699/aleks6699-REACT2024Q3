9552652b8bda4f95f1832932260508c1d5d44285
Oleksandr <aleks6699@gmail.com>
2024-07-04 17:32:55 +0300

    feat: add component not found

--- class-components/src/components/loading/loading.tsx
+++ class-components/src/components/loading/loading.tsx
@@ -1,14 +1,12 @@
 import { Component } from 'react';
 import './loading.css';
+import loading from '/loading.gif';
 
 export default class Loading extends Component {
   render() {
     return (
       <div className="loading">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
-          alt="loading"
-        />
+        <img src={loading} alt="loading" />
       </div>
     );
   }
--- class-components/src/components/main/result.tsx
+++ class-components/src/components/main/result.tsx
@@ -12,17 +12,23 @@ interface MainProps {
 
 export default class Main extends Component<MainProps> {
   render() {
+    const { results } = this.props;
+
     return (
       <main className="results">
-        {this.props.results.map((result, index) => (
-          <div className="result-item" key={index}>
-            <h2>{result.name}</h2>
-            <p> Period: {result.rotation_period}</p>
-            <p> Climate: {result.climate}</p>
-            <p> Terrain: {result.terrain}</p>
-            <p> Diameter: {result.diameter}</p>
-          </div>
-        ))}
+        {results.length === 0 ? (
+          <div className="found">No results found</div>
+        ) : (
+          results.map((result, index) => (
+            <div className="result-item" key={index}>
+              <h2>{result.name}</h2>
+              <p>Rotation Period: {result.rotation_period}</p>
+              <p>Climate: {result.climate}</p>
+              <p>Terrain: {result.terrain}</p>
+              <p>Diameter: {result.diameter}</p>
+            </div>
+          ))
+        )}
       </main>
     );
   }
