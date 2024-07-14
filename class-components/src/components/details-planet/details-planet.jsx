38670cee047f44b5abd94fbb8a792f36f0b2bab2
Oleksandr  Tsurkan <58092216+aleks6699@users.noreply.github.com>
2024-07-14 17:38:11 +0300

    Delete class-components directory

--- class-components/src/components/details-planet/details-planet.jsx
+++
@@ -1,9 +0,0 @@
-// export default function DetailsPlanet({ planet }) {
-//   return (
-//     <>
-//       <div className="details-planet">
-//         <h2>Details Planet</h2>
-//       </div>
-//     </>
-//   );
-// }
